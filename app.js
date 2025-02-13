29fd3e722d713a171e950185120146768ae629e1
Marios Michalopoulos <mariosfish1@gmail.com>
2025-02-13 22:36:52 +0200

    refactor: improve code formatting and consistency in app.js

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ mongoose.connect(process.env.MONGO_URI, {
     pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, function(err) {
+}, function (err) {
     if (err) {
         console.log("error!! " + err)
     } else {
-      //  console.log("MongoDB Connection Successful")
+        console.log("MongoDB Connection Successful")
     }
 })
 
@@ -38,11 +38,11 @@ var planetModel = mongoose.model('planets', dataSchema);
 
 
 
-app.post('/planet',   function(req, res) {
-   // console.log("Received Planet ID " + req.body.id)
+app.post('/planet', function (req, res) {
+    // console.log("Received Planet ID " + req.body.id)
     planetModel.findOne({
         id: req.body.id
-    }, function(err, planetData) {
+    }, function (err, planetData) {
         if (err) {
             alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
             res.send("Error in Planet Data")
@@ -52,12 +52,12 @@ app.post('/planet',   function(req, res) {
     })
 })
 
-app.get('/',   async (req, res) => {
+app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
 
-app.get('/os',   function(req, res) {
+app.get('/os', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "os": OS.hostname(),
@@ -65,14 +65,14 @@ app.get('/os',   function(req, res) {
     });
 })
 
-app.get('/live',   function(req, res) {
+app.get('/live', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "status": "live"
     });
 })
 
-app.get('/ready',   function(req, res) {
+app.get('/ready', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "status": "ready"
@@ -80,7 +80,7 @@ app.get('/ready',   function(req, res) {
 })
 
 app.listen(3000, () => {
-    console.log("Server successfully running on port - " +3000);
+    console.log("Server successfully running on port - " + 3000);
 })
 
 
